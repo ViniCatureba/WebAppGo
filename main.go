c8efd90883f4cfe803e8632a1cd8014b050d0bc1
ViniCatureba <vinicius.bigao14@gmail.com>
2023-02-27 10:46:51 -0300

    Db connection

--- main.go
+++ main.go
@@ -1,17 +1,64 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq"
 )
+
+func DataBaseConection() *sql.DB {
+	connection := "user=postgres dbname=Shoe_Store password=Vinivini14! host=localhost sslmode=disable"
+
+	db, err := sql.Open("postgres", connection)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+type Product struct {
+	id          int
+	Name        string
+	Description string
+	Price       float64
+	Amount      int
+}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	db := DataBaseConection()
+	selectProducts, err := db.Query("select * from shoes")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Product{}
+	products := []Product{}
+	for selectProducts.Next() {
+		var id, amount int
+		var name, description string
+		var price float64
+
+		err = selectProducts.Scan(&id, &name, &description, &price, &amount)
+		if err != nil {
+			panic(err.Error())
+		}
+		p.Name = name
+		p.Description = description
+		p.Price = price
+		p.Amount = amount
+		products = append(products, p)
+	}
+
+	temp.ExecuteTemplate(w, "Index", products)
+	defer db.Close()
 }
